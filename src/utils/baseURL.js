b2d4937185fc839dfe21f902ac4825a9a7119045
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-08-26 23:54:52 +0400

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 export const getBaseUrl = () => {
-    return 'http://localhost:5000';//رابط الباك اند بدل 5000
+    return 'https://collection-b.vercel.app';//رابط الباك اند بدل 5000
 }
 // export const getBaseUrl = () => {
 //     return 'https://www.henna-burgund.shop';//رابط الباك اند بدل 5000 رجع
